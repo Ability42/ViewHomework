55eb4d8e384e1de3d43769a5ba8525f05ec1615b
riko105 <stepanpaholyk@gmail.com>
2016-07-20 13:59:27 +0300

    Initial Commit

---
+++ ViewHomeworkClear/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewHomeworkClear
+//
+//  Created by Stepan Paholyk on 7/20/16.
+//  Copyright © 2016 Stepan Paholyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
