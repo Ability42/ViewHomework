b128dde3721198cc1c8ba18c7fa57d1bf86088e2
riko105 <stepanpaholyk@gmail.com>
2016-07-21 09:26:10 +0300

    Checkers added

--- ViewHomeworkClear/ViewController.m
+++ ViewHomeworkClear/ViewController.m
@@ -9,11 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (strong, nonatomic) UIView* boardView;
-@property (strong ,nonatomic) UIView* cellView; // needed Mutable Array for save cells view in heap
+@property (weak, nonatomic) UIView* boardView;
+@property (weak ,nonatomic) UIView* cellView; // needed Mutable Array for save cells view in heap
 @end
 
 @implementation ViewController
+
+#pragma mark - viewDidLoad
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -32,7 +34,7 @@
     boardRect.origin.y = (CGRectGetHeight(self.view.frame) - boardEdge)/2;
     boardRect.size = boardSize;
     
-    self.boardView = [self createViewWithRect:boardRect withColor:[UIColor brownColor] withParentView:self.view andMask:stableMask];
+    self.boardView = [self createViewWithRect:boardRect withColor:[UIColor whiteColor] withParentView:self.view andMask:stableMask];
     
     // cells create
     
@@ -43,17 +45,34 @@
     CGRect cellRect;
     cellRect.size = cellSize;
     
+    CGRect checkerRect = CGRectInset(cellRect, 12, 12);
+
     for (int rows = 0; rows < 8; rows++) {
         for (int columns = 0; columns < 8; columns++) {
             if ((rows + columns) % 2 != 0) {
                 cellRect.origin.x = rows*cellEdge;
                 cellRect.origin.y = columns*cellEdge;
-                [self createViewWithRect:cellRect withColor:[UIColor cyanColor] withParentView:self.boardView andMask:stableMask];
+                [self createViewWithRect:cellRect withColor:[UIColor brownColor] withParentView:self.boardView andMask:stableMask];
+            }
+            
+            if ((rows + columns)%2 != 0 && ((columns >= 0 && columns < 3) || (columns > 4 && columns < 8))) {
+                checkerRect.origin.x = cellRect.origin.x + (CGRectGetWidth(cellRect) - CGRectGetWidth(checkerRect))/2;
+                checkerRect.origin.y = cellRect.origin.y + (CGRectGetHeight(cellRect) - CGRectGetHeight(checkerRect))/2;
+                if (columns >= 0 && columns < 3) {
+                    [self createViewWithRect:checkerRect withColor:[UIColor yellowColor] withParentView:self.boardView andMask:stableMask];
+                } else {
+                    [self createViewWithRect:checkerRect withColor:[UIColor blueColor] withParentView:self.boardView andMask:stableMask];
+                }
             }
         }
     }
     
+    // make checkers (needed heap???)
+    
+    
 }
+
+#pragma mark - createViewWithRect
 
 - (UIView*) createViewWithRect:(CGRect)rect withColor:(UIColor*)color withParentView:(UIView*)parentView andMask:(UIViewAutoresizing)mask
 {
@@ -64,6 +83,8 @@
     
     return view;
 }
+
+#pragma mark Memory
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
